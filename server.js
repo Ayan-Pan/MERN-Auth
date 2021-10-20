5cf194a9f39e69852c128ee244058659dfe1e181
Ayan <ayanpan228@gmail.com>
2021-10-20 13:37:31 +0530

    MERN-Auth Uploaded

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const connectDB = require("./config/db");
+
+require("dotenv").config({ path: "./config.env" });
+
+const errorHandler = require("./middleware/error");
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+// Error Handler {should be last piece of middleware}
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Sever running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
